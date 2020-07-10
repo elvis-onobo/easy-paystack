45e913acd3165ebe61a1ea10b119c51c8532cb87
elvis-onobo <53485295+elvis-onobo@users.noreply.github.com>
2020-07-10 22:07:08 +0100

    Feat (Transactions): Added transaction initialization

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const env = require('dotenv').config()
 const verify = require('./lib/verification')
 const misc = require('./lib/miscellaneous')
+const transactions = require('./lib/transactions')
 
 const bvn = process.env['MY_BVN']
 const bin = process.env['CARD_BIN']
@@ -11,6 +12,7 @@ const bin = process.env['CARD_BIN']
  * Bundle all the methods into one object
  */
 const Paystack = {
+	transactions,
 	verify,
 	misc
 }
@@ -21,3 +23,17 @@ const Paystack = {
 module.exports = Paystack
 
 
+const initializeTrx = Paystack.transactions.initialize(200, 'rapknowlogy@gmail.com')
+
+initializeTrx.then(data => console.log(data))
+	.catch(err => console.log(err))
+
+/*
+ * THIS FUNCTIONALITY APPEARS TO BE DEPRECATED BY PAYSTACK
+ * @ params: Phone Number e.g 234802222222, Callback URL
+ * Verify a customer by his phone number through Truecaller
+ */
+// const resolvedPhoneNumber = Paystack.verify.resolvePhoneNumber(2348039101861, 'google.com')
+
+// resolvedPhoneNumber.then(data => console.log(data))
+// 	.catch(err => console.log(err))
